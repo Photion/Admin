e1e9b9da787de6cf19a33c5ca0f4cd71cf681b0d
William Di Pasquale <wdp@dipasqualew.com>
2020-10-11 10:34:34 +0100

    fix: Tweak fragment card (#170)

--- src/vue/components/ui/forms/PhoBoolean.vue
+++ src/vue/components/ui/forms/PhoBoolean.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="pho-switch-wrapper">
+    <div v-if="label && !inline">
+      <label :for="id.unique" class="pho-label">
+        {{ label }}
+      </label>
+    </div>
     <div class="pho-switch-container">
       <div class="pho-switch">
         <input
           type="checkbox"
           :id="id.unique"
+          :checked="modelValue"
           :value="modelValue"
           @input="onInput"
           :name="name"
           :cy="id.family" />
         <label :for="id.unique" class="pho-switch-circle"></label>
       </div>
-      <label :for="id.unique" class="pho-switch-text">{{ label }}</label>
+      <label v-if="label && inline" :for="id.unique" class="pho-switch-text">{{ label }}</label>
     </div>
   </div>
 </template>
@@ -27,6 +33,10 @@ export default defineComponent({
   props: {
     ...componentProps,
     ...getFormProps(Boolean),
+    inline: {
+      type: Boolean,
+      default: () => false,
+    },
   },
 
   setup(props, context) {
